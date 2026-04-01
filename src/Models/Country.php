28e5e75d6325e937b70302da01083fe99a063b57
Ronald <ronald.jian@gmail.com>
2026-04-02 04:55:36 +0800

    add country modal

---
+++ src/Models/Country.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Sharenjoy\NoahShop\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Sharenjoy\NoahCms\Models\Traits\CommonModelTrait;
+
+class Country extends Model
+{
+    use CommonModelTrait;
+
+    protected $casts = [];
+
+    protected array $sort = [];
+
+    /** RELACTIONS */
+
+    /** SCOPES */
+
+    /** EVENTS */
+
+    /** SEO */
+
+    /** OTHERS */
+
+}
