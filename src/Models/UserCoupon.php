14a0e80c6ac72cff92ca751abfdb4e66feff8357
Ronald <ronald.jian@gmail.com>
2026-04-09 23:24:35 +0800

    user coupon scopes

--- src/Models/UserCoupon.php
+++ src/Models/UserCoupon.php
@@ -2,12 +2,13 @@
 
 namespace Sharenjoy\NoahShop\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Sharenjoy\NoahShop\Models\Promo;
 use Sharenjoy\NoahCms\Models\Traits\CommonModelTrait;
+use Sharenjoy\NoahShop\Models\Promo;
 use Sharenjoy\NoahShop\Models\User;
 use Sharenjoy\NoahShop\Models\UserCouponStatus;
 use Spatie\Activitylog\Traits\LogsActivity;
@@ -48,5 +49,47 @@ class UserCoupon extends Model
     public function userCouponStatuses(): HasMany
     {
         return $this->hasMany(UserCouponStatus::class);
+    }
+
+    /** SCOPES */
+
+    // 可使用的優惠券
+    public function scopeAvailableCoupons($query): Builder
+    {
+        // 在user_coupon_statuses裡最新的status不等於used的優惠券，且大於started_at，小於expired_at的優惠券
+        return $query
+        ->where('started_at', '<=', now())
+        ->where('expired_at', '>=', now())
+        ->whereHas('userCouponStatuses', function ($query) {
+            $query->latest()->where('status', '!=', 'used');
+        });
+    }
+
+    // 即將到期的優惠券
+    public function scopeExpiringSoonCoupons($query): Builder
+    {
+        // 可使用的優惠券，但3天內就要過期了
+        return $query->availableCoupons()->whereBetween('expired_at', [now(), now()->addDays(3)]);
+    }
+
+    // 已使用的優惠券
+    public function scopeUsedCoupons($query): Builder
+    {
+        // 在user_coupon_statuses裡最新的status等於used的優惠券
+        return $query
+        ->whereHas('userCouponStatuses', function ($query) {
+            $query->latest()->where('status', 'used');
+        });
+    }
+
+    // 已過期的優惠券
+    public function scopeExpiredCoupons($query): Builder
+    {
+        // 超過expired_at日期的優惠券，而且在user_coupon_statuses裡最新的status不等於used的優惠券
+        return $query
+        ->where('expired_at', '<', now())
+        ->whereHas('userCouponStatuses', function ($query) {
+            $query->latest()->where('status', '!=', 'used');
+        });
     }
 }
